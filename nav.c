1f8b3dba4fb515a4547f94471706b9e7f8d9dcaf
dwunger <106967706+dwunger@users.noreply.github.com>
2023-10-15 03:15:22 -0400

    bob ross & happy little seg faults

--- nav.c
+++ nav.c
@@ -2,10 +2,31 @@
 #include <stdio.h>
 #include <time.h>
 
-#define UP_KEY    L'↑'
-#define DOWN_KEY  L'↓'
-#define LEFT_KEY  L'←'
-#define RIGHT_KEY L'→'
+#define UP_SYMBOL    '^'
+#define DOWN_SYMBOL  'v'
+#define LEFT_SYMBOL  '<'
+#define RIGHT_SYMBOL '>'
+
+// and so all your directions are now become mine
+char convert_direction_to_vim_mapping(char *direction) {
+    switch (*direction)
+    {
+    case LEFT_SYMBOL:
+        *direction = 'h'; 
+        break;
+    case RIGHT_SYMBOL:
+        *direction = 'l'; 
+        break;
+    case UP_SYMBOL:
+        *direction = 'k'; 
+        break;
+    case DOWN_SYMBOL:
+        *direction = 'j'; 
+        break;
+    default:
+        break;
+    }
+}
 
 int main(void) {
     HANDLE user_input = GetStdHandle(STD_INPUT_HANDLE);
@@ -21,31 +42,26 @@ int main(void) {
     // Set console mode to UTF-16 (Windows uses UTF-16 for Unicode)
     SetConsoleOutputCP(CP_UTF8);
 
-    wchar_t array[4] = {L'↑', L'↓', L'←', L'→'};
+    char array[4] = {UP_SYMBOL, DOWN_SYMBOL, LEFT_SYMBOL, RIGHT_SYMBOL};
     size_t length = sizeof(array)/sizeof(array[0]);
 
     char c;
     while (1) {
         srand((unsigned int) time(NULL));
-        wchar_t expected_input = array[rand() % length];
-        wprintf(L"%lc\n", expected_input);
-        if (expected_input = UP_KEY) {
-            expected_input = 'k';
-        }
-        if (expected_input = DOWN_KEY) {
-            expected_input = 'j';
-        }
-        if (expected_input = LEFT_KEY) {
-            expected_input = 'h';
-        }
-        if (expected_input = RIGHT_KEY) {
-            expected_input = 'l';
-        }
-        if (ReadFile(user_input, &c, 1, &bytesRead, NULL) && bytesRead == 1) {
-            if (c == 'q') break;  // Quit on 'q'
-            printf("You pressed: %c\n", c);
+        char *direction_symbol = array[rand() % length];
+        printf("%c\n", *direction_symbol);
+        convert_direction_to_vim_mapping(*direction_symbol);
+        if (ReadFile(user_input, &c, sizeof(char), &bytesRead, NULL) && bytesRead == sizeof(char)) {
+            if (c == 'q') { 
+            break;  // Quit on 'q'
+            }
+            else if ( c != *direction_symbol) {
+                puts("get good");
+            }
 
+            printf("You pressed: %c\n", c);
         }
+
     }
 
     // Restore original mode
